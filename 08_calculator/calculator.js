b6ef4e109cb796b347a77bb6667800ce14a7bd05
Santhosh Sivakulanathan <santhoshsivakulanathan@gmail.com>
2023-03-20 00:25:50 -0700

    exercises 7 - 11

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for(let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let ans = arr[0];
+  for(let i = 1; i < arr.length; i++){
+    ans *= arr[i];
+  }
+  return ans;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let ans = num;
+  for(let i = 0; i < power - 1; i++){
+    ans *= num;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let ans = 1;
+  for(let i = 1; i < num+1; i++){
+    ans *= i;
+  }
+  return ans;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (arg) {
+  let str = arg.toLowerCase().replace(/[^a-z]/g, "");
+  let reverse = "";
+  for(let i = str.length -1; i >= 0; i--){
+    reverse += str[i];
+  }
+  if(str == reverse) return true;
+  else return false;
 };
 
 // Do not edit below this line
